a5f8179040edab734ddc1d42355b399c7c19ec86
AlifArshad04 <arshadbakshi@iut-dhaka.edu>
2022-07-27 03:07:10 +0600

    added rover in window

--- difdrive.py
+++ difdrive.py
@@ -1,3 +1,5 @@
+from time import sleep
+from turtle import width
 import pygame
 import math
 
@@ -21,6 +23,29 @@ class Envir:
         self.map = pygame.display.set_mode((self.width, self.hight))
 
 
+class Rover:
+    def __init__(self, startpos, roverImg, width):
+        self.m2p = 3779.52  # meter to pixels
+
+        # rover dimentions
+        self.w = width
+        self.x = startpos[0]
+        self.y = startpos[1]
+        self.theta = 0
+        self.vl = 0.01*self.m2p  # m/s
+        self.vr = 0.01*self.m2p
+        self.maxspeed = 0.02*self.m2p
+        self.minspeed = 0.02*self.m2p
+
+        # graphics
+        self.img = pygame.image.load(roverImg)
+        self.rotated = self.img
+        self.rect = self.rotated.get_rect(center=(self.x, self.y))
+
+    def draw(self, map):
+        map.blit(self.rotated, self.rect)
+
+
 # initialisation
 pygame.init()
 
@@ -36,6 +61,9 @@ running = True
 # the envir
 envir = Envir(dims)
 
+# the rover
+rover = Rover(
+    start, "/home/alif/Projects/path_planning/rover.png", 0.01*3779.52)
 
 # simulation loop
 while running:
@@ -46,3 +74,4 @@ while running:
 
     pygame.display.update()
     envir.map.fill(envir.black)
+    rover.draw(envir.map)
